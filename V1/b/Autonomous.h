5a2987d4d36f3f64f4945f9f8d05bd28ecb8fffe
tylerfilla <tylerfilla@gmail.com>
2014-07-22 16:40:50 -0500

    Restructured Autonomous V1 and added a missing revision

---
+++ V1/b/Autonomous.h
@@ -0,0 +1,13 @@
+
+// The Doppler Effect
+// 2013-14 Autonomous Program
+// Header File
+
+// File: "Autonomous.h"
+// Created: 10/24/2013
+// Modified: 10/24/2013
+
+#define DEFAULT_DRIVE_SMOOTHEN false
+
+void driveForTime(int power, int time);
+void driveForTime(int power, int time, bool smoothen);
